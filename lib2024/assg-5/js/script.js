24984ade6831069c9a46fadc975c70cf0303bd9c
Tristan <Chan0429@algonquinlive.com>
2022-04-15 21:18:21 -0400

    Update script.js

---
+++ lib2024/assg-5/js/script.js
@@ -0,0 +1,21 @@
+// SEQUENCE OF JS
+// 1) User Influence
+// 2) Event Interacts
+// 3) Function is Called
+// 4) Script runs
+
+window.onload = init;
+
+function name_of_function() {
+
+}
+
+function init(){
+
+}
+
+function myFunction() {
+	document.getElementById("demo") .innerHTML = "Paragraph changed.";
+}
+
+<script src="myScript.js"></script>
